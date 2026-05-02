dcad9010ff59a9315c1c0ef248010136d991289c
TANGSHUHAN77 <165114756+TANGSHUHAN77@users.noreply.github.com>
2026-05-02 19:35:41 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const RazakEventApp());
+  runApp(const MyApp());
 }
 
-class RazakEventApp extends StatelessWidget {
-  const RazakEventApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RazakEvent',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('RazakEvent Firebase Connected'),
-        ),
-      ),
+    return const MaterialApp(
+      home: LoginPage(),
     );
   }
 }
